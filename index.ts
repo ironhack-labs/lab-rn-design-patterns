cf9272dad0a54a6424c620092905c02bd7428980
PabloGastelum-Femsa <jose.gastelum@digitalfemsa.com>
2023-08-03 15:09:01 -0700

    Observer pattern implemented

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import Logger from './logger';
-import ProductFactory from './productFactory';
-import Facade from './facade';
-
+import Logger from "./logger";
+import ProductFactory from "./productFactory";
+import Facade from "./facade";
+import { Subject, Observer } from "./observer";
 
 const logger1 = Logger.getInstance();
 const logger2 = Logger.getInstance();
@@ -17,14 +17,13 @@ logger2.printLogs();
 
 const factory = new ProductFactory();
 
-const productA = factory.createProduct('A');
-const productB = factory.createProduct('B');
+const productA = factory.createProduct("A");
+const productB = factory.createProduct("B");
 
 console.log(productA.operation()); // Output: "Result of ConcreteProductA."
 console.log(productB.operation()); // Output: "Result of ConcreteProductB."
 
 // // Iteration 3:
-
 
 const facade = new Facade();
 
@@ -34,33 +33,33 @@ const resultC = facade.operationC();
 
 console.log(resultA); // Output: "SubsystemA: operation A1. SubsystemA: operation A2."
 console.log(resultB); // Output: "SubsystemB: operation B1. SubsystemB: operation B2."
-console.log(resultC); 
+console.log(resultC);
 
-// // Iteration 4: 
+// Iteration 4:
 
-// class ConcreteObserver implements Observer {
-//   public update(data: any): void {
-//     console.log(`Received update with data: ${data}`);
-//   }
-// }
+class ConcreteObserver implements Observer {
+  public update(data: any): void {
+    console.log(`Received update with data: ${data}`);
+  }
+}
 
-// const subject = new Subject();
+const subject = new Subject();
 
-// const observer1 = new ConcreteObserver();
-// const observer2 = new ConcreteObserver();
+const observer1 = new ConcreteObserver();
+const observer2 = new ConcreteObserver();
 
-// subject.addObserver(observer1);
-// subject.addObserver(observer2);
+subject.addObserver(observer1);
+subject.addObserver(observer2);
 
-// subject.someBusinessLogic();
-// // Output:
-// // Doing some business logic...
-// // Received update with data: Some data to be sent to observers.
-// // Received update with data: Some data to be sent to observers.
+subject.someBusinessLogic();
+// Output:
+// Doing some business logic...
+// Received update with data: Some data to be sent to observers.
+// Received update with data: Some data to be sent to observers.
 
-// subject.removeObserver(observer1);
+subject.removeObserver(observer1);
 
-// subject.someBusinessLogic();
-// // Output:
-// // Doing some business logic...
-// // Received update with data: Some data to be sent to observers.
+subject.someBusinessLogic();
+// Output:
+// Doing some business logic...
+// Received update with data: Some data to be sent to observers.
