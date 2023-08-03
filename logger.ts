ee6514b2e2ff2c87e9f899514ac687a400125988
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-08-03 15:32:31 -0700

    Iteration 1: Implement a Singleton

--- logger.ts
+++ logger.ts
@@ -1,25 +1,29 @@
 class Logger {
-  private static instance: Logger;
-  private logs: string[];
+  private static instance: Logger
+  private logs: string[]
 
   private constructor() {
-    this.logs = [];
+    this.logs = []
   }
 
   public static getInstance() {
-    // TODO: Implement the Singleton pattern here
+    if (!this.instance) {
+      this.instance = new Logger()
+    }
+
+    return this.instance
   }
 
   public log(message: string): void {
-    this.logs.push(message);
+    this.logs.push(message)
   }
 
   public printLogs(): void {
-    console.log("Logs:");
+    console.log('Logs:')
     for (const log of this.logs) {
-      console.log(log);
+      console.log(log)
     }
   }
 }
 
-export default Logger;
+export default Logger
