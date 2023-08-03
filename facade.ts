4dafc894f494113ca442f87149f21ebde2de4560
Jesus Gutierrez <jesus.gutierrez@digitalfemsa.com>
2023-08-03 17:43:15 -0500

    done

--- facade.ts
+++ facade.ts
@@ -2,6 +2,26 @@ import { SubsystemA, SubsystemB, SubsystemC } from './subsystems';
 
 class Facade {
   // TODO: Implement the Facade class using the Facade pattern
+  subSystemA: SubsystemA;
+  subSystemB: SubsystemB;
+  subSystemC: SubsystemC;
+  constructor(){
+    this.subSystemA = new SubsystemA();
+    this.subSystemB = new SubsystemB();
+    this.subSystemC = new SubsystemC();
+  }
+
+  operationA(){
+    return `${this.subSystemA.operationA1()} ${this.subSystemA.operationA2()}`
+  };
+
+  operationB(){
+    return `${this.subSystemB.operationB1()} ${this.subSystemB.operationB2()}`
+  }
+
+  operationC(){
+    return `${this.subSystemC.operationC1()} ${this.subSystemC.operationC2()}`
+  }
 }
 
 export default Facade;
