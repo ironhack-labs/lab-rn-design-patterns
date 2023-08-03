4449f84fdaa246e0e462f88be3d1dd9281f7da2b
eduardoviscencio <eduardoviscencio.dev@gmail.com>
2023-08-03 16:13:10 -0600

    feat: complete iteration 1

--- logger.ts
+++ logger.ts
@@ -1,25 +1,29 @@
 class Logger {
-  private static instance: Logger;
-  private logs: string[];
+  private static instance: Logger
+  private logs: string[]
 
-  private constructor() {
-    this.logs = [];
+  private constructor () {
+    this.logs = []
   }
 
-  public static getInstance() {
-    // TODO: Implement the Singleton pattern here
+  public static getInstance () {
+    if (!Logger.instance) {
+      Logger.instance = new Logger()
+    }
+
+    return Logger.instance
   }
 
-  public log(message: string): void {
-    this.logs.push(message);
+  public log (message: string): void {
+    this.logs.push(message)
   }
 
-  public printLogs(): void {
-    console.log("Logs:");
+  public printLogs (): void {
+    console.log('Logs:')
     for (const log of this.logs) {
-      console.log(log);
+      console.log(log)
     }
   }
 }
 
-export default Logger;
+export default Logger
