b5d5503d587dbe7b789e5498f016d6cb251e4d1c
José De Ita <jose.deita@digitalfemsa.com>
2023-08-03 17:23:04 -0500

    Iteration 4: Implement an Observer

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { testSingleton } from "./logger";
 import { testFactory } from "./productFactory";
 import { testFacade } from "./facade";
+import { testObserver } from "./observer";
 
 testSingleton();
 testFactory();
 testFacade();
+testObserver();
