85bebf08512698118c1888fa385a87d1361fb342
Rubén Capetillo <jose.capetillo@digitalfemsa.com>
2023-08-03 18:47:24 -0600

    feat(*): iteration2: factory pattern

--- productFactory.ts
+++ productFactory.ts
@@ -1,7 +1,17 @@
 // TODO: Implement the ProductFactory class using the Factory pattern
 
 class ProductFactory {
-  // TODO: Implement the factory method that creates and returns Product instances
+  public createProduct(type: string): Product {
+    switch(type) {
+      case 'A':
+        return new ConcreteProductA();
+      case 'B':
+        return new ConcreteProductB();
+
+      default:
+        throw new Error('Invalid product type');
+    }
+  }
 }
 
 export default ProductFactory;
