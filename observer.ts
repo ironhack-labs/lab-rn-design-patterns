be4effd142a515e60fa4e6f6636946d650f1fc7f
Atza Botello <alberto.botello@digitalfemsa.com>
2023-08-04 04:46:31 -0600

    done

--- observer.ts
+++ observer.ts
@@ -1,20 +1,28 @@
-interface Observer {
+export interface Observer {
   update(data: any): void;
 }
 
-class Subject {
+export class Subject {
   private observers: Observer[] = [];
 
   public addObserver(observer: Observer): void {
     // TODO: Implement this method to add an observer to the list
+    this.observers.push(observer);
   }
 
   public removeObserver(observer: Observer): void {
     // TODO: Implement this method to remove an observer from the list
+    const index = this.observers.indexOf(observer);
+    if (index!== -1) {
+      this.observers.splice(index, 1);
+    }
   }
 
   public notifyObservers(data: any): void {
     // TODO: Implement this method to notify all observers
+    for (const observer of this.observers) {
+      observer.update(data);
+    }
   }
 
   public someBusinessLogic(): void {
@@ -22,3 +30,9 @@ class Subject {
     this.notifyObservers("Some data to be sent to observers.");
   }
 }
+
+/**
+ * The Observer class
+ * we added implementatios for addObserver() and removeObserver() and notifyObservers().
+ * the notifyObservers() method will notify all observers. using the update method registered
+ */
