18cc55a524a197900b4a39e31df925b2135e4e52
carlos <carlos.pacheco@digitalfemsa.com>
2023-08-04 13:25:37 -0500

    done

--- observer.ts
+++ observer.ts
@@ -1,20 +1,22 @@
-interface Observer {
+export interface Observer {
   update(data: any): void;
 }
 
-class Subject {
+export class Subject {
   private observers: Observer[] = [];
 
   public addObserver(observer: Observer): void {
-    // TODO: Implement this method to add an observer to the list
+   this.observers.push(observer)
   }
 
   public removeObserver(observer: Observer): void {
     // TODO: Implement this method to remove an observer from the list
+    this.observers = this.observers.filter((item) => item !== observer)
   }
 
   public notifyObservers(data: any): void {
     // TODO: Implement this method to notify all observers
+    this.observers.forEach((item) => item.update(data))
   }
 
   public someBusinessLogic(): void {
--- productFactory.ts
+++ productFactory.ts
@@ -1,7 +1,15 @@
 // TODO: Implement the ProductFactory class using the Factory pattern
 
+import { ConcreteProductA, ConcreteProductB, Product } from "./product";
+
 class ProductFactory {
   // TODO: Implement the factory method that creates and returns Product instances
+  createProduct(name : "A" | "B") : Product {
+    if(name === "A"){
+      return new ConcreteProductA()
+    }
+    return new ConcreteProductB()
+  }
 }
 
 export default ProductFactory;
