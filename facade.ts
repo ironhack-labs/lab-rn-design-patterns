e46a5de2f22877f1796fff811da5b536ee2921aa
Rubén Capetillo <jose.capetillo@digitalfemsa.com>
2023-08-03 18:53:49 -0600

    feat(*): iteration 3: facade pattern

--- facade.ts
+++ facade.ts
@@ -1,7 +1,27 @@
 import { SubsystemA, SubsystemB, SubsystemC } from './subsystems';
 
 class Facade {
-  // TODO: Implement the Facade class using the Facade pattern
+  private subsystemA: SubsystemA;
+  private subsystemB: SubsystemB;
+  private subsystemC: SubsystemC;
+
+  constructor() {
+    this.subsystemA = new SubsystemA();
+    this.subsystemB = new SubsystemB();
+    this.subsystemC = new SubsystemC();
+  }
+
+  public operationA(): string {
+    return this.subsystemA.operationA1() + " " + this.subsystemA.operationA2();
+  }
+
+  public operationB(): string {
+    return this.subsystemB.operationB1() + " " + this.subsystemB.operationB2();
+  }
+
+  public operationC(): string {
+    return this.subsystemC.operationC1() + " " + this.subsystemC.operationC2();
+  }
 }
 
 export default Facade;
