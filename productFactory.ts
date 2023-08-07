f9da2aefc7da68c10bd475521897d539d59ff0c2
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-06 22:46:15 -0500

    refactor(product): second iteration factory

--- productFactory.ts
+++ productFactory.ts
@@ -1,7 +1,17 @@
 // TODO: Implement the ProductFactory class using the Factory pattern
+import { ConcreteProductA, ConcreteProductB, Product } from "./product";
 
 class ProductFactory {
   // TODO: Implement the factory method that creates and returns Product instances
+  public createProduct(productType: "A" | "B"): Product {
+    if (productType === "A") {
+      return new ConcreteProductA();
+    } else if (productType === "B") {
+      return new ConcreteProductB();
+    } else {
+      throw new Error("Invalid product type.");
+    }
+  }
 }
 
 export default ProductFactory;
